985bfa71814a88c9738446b4ea5cdf37e4a9d46b
11nstz <1361125611@qq.com>
2020-06-22 19:48:44 +0800

    fourth

--- EFGetStarted/Model.cs
+++ EFGetStarted/Model.cs
@@ -9,6 +9,22 @@ namespace EFGetStarted
         public DbSet<CERT_Student> students { get; set; }
         public DbSet<CERT_teacher> teachers { get; set; }
         public DbSet<CERT_course> courses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CERT_Student>().HasKey(c => new { c.StudentId });//设置student主键
+            modelBuilder.Entity<CERT_Student>().Property(t => t.Name).HasColumnType("varchar(25)").IsRequired();//设置name类型
+            modelBuilder.Entity<CERT_Student>().Property(teachers => teachers.Sex).HasColumnType("varchar(25)").IsRequired();//设置性别类型
+
+            modelBuilder.Entity<CERT_teacher>().HasKey(c => new { c.TeacherId });//设置teacher主键
+            modelBuilder.Entity<CERT_teacher>().Property(t => t.Name).HasColumnType("varchar(25)").IsRequired();
+            modelBuilder.Entity<CERT_teacher>().Property(t => t.Age).HasColumnType("vinyint").IsRequired();
+            modelBuilder.Entity<CERT_teacher>().Property(t => t.Sex).HasColumnType("varchar(25)").IsRequired();
+
+            modelBuilder.Entity<CERT_course>().HasKey(c => new { c.ID });
+            modelBuilder.Entity<CERT_course>().Property(t=>t.courseday).HasColumnType("varchar(25)").IsRequired();
+            modelBuilder.Entity<CERT_course>().Property(t=>t.coursename).HasColumnType("varchar(25)").IsRequired();
+            modelBuilder.Entity<CERT_course>().Property(t=>t.coursepoint).HasColumnType("vinyint").IsRequired();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Blogging;Trusted_Connection=True;");
@@ -18,7 +34,7 @@ namespace EFGetStarted
     [Table("CERT_Student", Schema = "11nstz")]
     public class CERT_Student
     {
-        [Key, Required]
+
         public int StudentId { get; set; }
         public string Name { get; set; }
         public string Sex { get; set; }
@@ -26,20 +42,16 @@ namespace EFGetStarted
     [Table("CERT_teacher", Schema = "11nstz")]
     public class CERT_teacher
     {
-        [Key, Required]
         public int TeacherId { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Required]
         public int Age { get; set; }
-
+        public string Sex { get; set; }
     }
     [Table("CERT_course", Schema = "11nstz")]
     public class CERT_course
     {
-
-        [Required, Key]
-        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }//标记数量
         public string courseday
         {
             get { return courseday; }
@@ -49,7 +61,7 @@ namespace EFGetStarted
             }
 
         }//第几天
-        public int coursenumber { get; set; }//标注在第几节课
+        public int coursepoint { get; set; }//标注在第几节课
         public string coursename { get; set; }
         public enum day
         {
